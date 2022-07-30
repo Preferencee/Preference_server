5961ca2c05df0849fddee3545a35e16d20ebef9b
KSJ0128 <ksj01128@naver.com>
2022-07-30 20:30:26 +0900

    회원가입&검색 구현

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'); // express 패키지 사용
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+const port = 3306;
+app.listen(port, () => {
+    console.log("Server listening on port", port)
+})
+
+app.use('/users', require('./routes/userRouter'));
